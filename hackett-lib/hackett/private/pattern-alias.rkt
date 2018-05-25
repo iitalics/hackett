230eb6c67825012cf34d1da9ef86a2371e8c7e26
Milo Turner <iitalics@gmail.com>
2018-05-25 18:31:34 -0400

    Fix missing requires

--- hackett-lib/hackett/private/pattern-alias.rkt
+++ hackett-lib/hackett/private/pattern-alias.rkt
@@ -3,9 +3,13 @@
 (provide pattern list)
 
 (require syntax/parse/define
+         (only-in hackett/private/adt defn)
+         (only-in hackett/private/prim/type Nil ::)
          (for-syntax racket/base
+                     racket/syntax
                      syntax/parse
-                     hackett/private/prop-case-pattern-expander))
+                     hackett/private/prop-case-pattern-expander
+                     hackett/private/util/stx))
 
 (begin-for-syntax
   (struct proc+case-pat-exp [proc case-pat-trans]
@@ -15,7 +19,7 @@
 
 (define-simple-macro
   (pattern (head:id arg:id ...) old:expr)
-  #:with head-internal (generate-temporaries #'head)
+  #:with head-internal (generate-temporary #'head)
   (begin
     (defn head-internal [[arg ...] old])
     (define-syntax head
@@ -30,4 +34,3 @@
            [(list)          #'Nil]
            [(list a bs ...) #'(:: a (list bs ...))])])
     (proc+case-pat-exp trans trans)))
-
